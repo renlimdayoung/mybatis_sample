0b57a28191416f5d61bf78213af51b184548f0fa
ldy <tlgurdlsn37@naver.com>
2018-02-08 17:33:20 +0900

    mybatis_sample gradle/ignore/applicationproperties/log4j2/mybatis-config init

---
+++ src/test/java/kr/or/dgit/mybatis_sample/MyBatisSqlSessionFactoryTest.java
@@ -0,0 +1,30 @@
+package kr.or.dgit.mybatis_sample;
+
+import static org.junit.Assert.*;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyBatisSqlSessionFactoryTest {
+	private static SqlSessionFactory factory;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		factory = MybatisSqlSessionFactory.getSqlSessionFactory();
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		factory = null;
+	}
+
+	@Test
+	public void test() {
+		Assert.assertNotNull(factory);
+	}
+
+}
