f01660dcd5d12cd42768943067eff4502a39f207
ldy <tlgurdlsn37@naver.com>
2018-02-13 09:59:48 +0900

    master

---
+++ src/main/java/kr/or/dgit/mybatis_sample/dao/StudentDao.java
@@ -0,0 +1,27 @@
+package kr.or.dgit.mybatis_sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.or.dgit.mybatis_sample.dto.Student;
+
+public interface StudentDao {
+	//select
+	Student selectStudentByNo(Student student);
+	List<Student> selectStudentByAll();
+	
+	List<Student> selectStudentByAllForResultMap();
+	List<Map<String, Object>> selectStudentByAllForHashMap();
+	
+	Student selectStudentByNoForResultMapExtends(Student student);
+	
+	Student selectStudentByNoAssociation(Student student);
+	
+	//insert
+	int insertStudent(Student student);
+	//update
+	int updateStudent(Student studeent);
+	//delete
+	int deleteStudent(int studId);
+	
+}
